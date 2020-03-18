780b0a2bfa48886f1d3543cbc11fd2d3bf85769c
Martin <martin.le-morvan@epitech.eu>
2020-03-18 14:56:50 +0100

    <feat> <add modified files> push error_handling modifications for option perfect and imperfect

--- generator/error_handling.c
+++ generator/error_handling.c
@@ -18,7 +18,7 @@ static int is_a_digit(char *nb)
 
 int error_handling(int ac, char *av[])
 {
-    if (ac > 3) {
+    if (ac > 4) {
         write(2, "Too many arguments\n", 20);
         return 84;
     } else if (ac < 3) {
